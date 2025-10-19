8a4939123220cb18835ee87dc685ee26ead45eac
vaturn <will6867@naver.com>
2025-10-20 00:21:54 +0900

    feat(InputHandler): implement input section

---
+++ src/main/java/calculator/InputHandler.java
@@ -0,0 +1,15 @@
+package calculator;
+
+import java.util.Scanner;
+
+// 프로그램의 입력을 담당
+// 한줄을 읽고 String 형태로 반환
+
+public class InputHandler {
+    public static String input(){
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
+
+        return scanner.nextLine();
+    }
+}
