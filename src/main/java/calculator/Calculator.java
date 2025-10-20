7dd4e55ae91dc37e1dfecf2da34478d48355d74f
vaturn <will6867@naver.com>
2025-10-20 17:36:32 +0900

    chore(Calculator): correct typo

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,11 +3,11 @@ package calculator;
 import java.util.List;
 
 public class Calculator {
-    public static int calculate(List<Integer> arryNums){
+    public static int calculate(List<Integer> arrayNums){
 
         int sum = 0;
 
-        for(int num: arryNums)
+        for(int num: arrayNums)
             sum += num;
         return sum;
     }
