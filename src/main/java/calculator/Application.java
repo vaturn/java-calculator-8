63ac28f6644533024c7116b70737ee9db562c590
vaturn <will6867@naver.com>
2025-10-20 16:53:07 +0900

    feat(Application): add Exception struct

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,17 +5,22 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            // 입력
+            String inputs = InputHandler.input();
 
-        // 입력
-        String inputs = InputHandler.input();
+            // 파싱
+            List<Integer> numList = Parser.parse(inputs);
 
-        // 파싱
-        List<Integer> numList = Parser.parse(inputs);
+            // 계산
+            int results = Calculator.calculate(numList);
 
-        // 계산
-        int results = Calculator.calculate(numList);
-
-        // 출력
-        OutputHandler.output(results);
+            // 출력
+            OutputHandler.output(results);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
