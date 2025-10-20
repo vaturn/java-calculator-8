279bf663387f320364111b4ebe30fc525857e43c
vaturn <will6867@naver.com>
2025-10-20 16:45:26 +0900

    feat(Application): implement main structure

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 입력
+        String inputs = InputHandler.input();
+
+        // 파싱
+        List<Integer> numList = Parser.parse(inputs);
+
+        // 계산
+        int results = Calculator.calculate(numList);
+
+        // 출력
+        OutputHandler.output(results);
     }
 }
