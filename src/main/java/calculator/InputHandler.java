807044dbd4895bcdd4828a1e27382b3f4092c7eb
vaturn <will6867@naver.com>
2025-10-20 19:12:21 +0900

    fix(InputHandler): modify output function

--- src/main/java/calculator/InputHandler.java
+++ src/main/java/calculator/InputHandler.java
@@ -1,18 +1,22 @@
 package calculator;
 
 import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 // 프로그램의 입력을 담당
 // 한줄을 읽고 String 형태로 반환
 
 public class InputHandler {
     public static String input(){
-        Scanner scanner = new Scanner(System.in);
-
-        if (scanner.hasNextLine()) {
-            return scanner.nextLine();
-        } else {
-            return "";
+        String inputs;
+        try {
+            inputs = Console.readLine();
         }
+        catch (Exception e){
+            inputs = "";
+        }
+
+        return inputs;
+
     }
 }
